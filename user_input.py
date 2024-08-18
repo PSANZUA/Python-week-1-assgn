17c3d88881eee951227e229319f90fd39ccb1764
Pauline Sanzua <paulinesanzua@gmail.com>
2024-08-18 13:09:32 +0300

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -4,8 +4,8 @@ name = input("What is your name? ")
 # Ask the user for their age 
 age = input("How old are you? ")
 
-#Ask the user for their location
+# Ask the user for their location
 location = input("Where do you live? ")
 
-Print out a personalized message
+# Print out a personalized message
 print(f"Hello {name},line you are {age} years old and live in {location}.")
